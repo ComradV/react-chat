79b6d4fa3e1a066c3cbd7e644e6ac2abab788acd
Gleb Vasiliev <vasiliev.g.i@gmail.com>
2018-10-07 12:14:55 +0300

    Start dev in Material-UI

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './testp.css';
-import MyComponent from './MyComponent'
-import Button from '@material-ui/core/Button';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro1">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <MyComponent greeting="Hi! Hi!!!">Child</MyComponent>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-    </div>
+      <div>
+        <h1>Hello app!</h1>
+      </div>
     );
   }
 }
